5f02e4f3769946c2ce317e4b9df0c1fe519a7b46
StevenWilliams198318 <StevenWilliams198318@gmail.com>
2024-08-26 22:14:49 +0200

    fixing merging error?

--- 50Projects/expanding-cards/script.js
+++ 50Projects/expanding-cards/script.js
@@ -1,17 +1,22 @@
-//Selecting all HTML elements with class 'panel' assign to panels.
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel') // 1
 
-//Iterating and adding event listener to each panel, and 'active' class to current 'panel' element.
-panels.forEach(panel => {
+panels.forEach(panel => { 
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
-})
+}) // 2
 
-// Removes the 'active' class from all the panels
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
-}
+} // 3
+
+
+/*
+Notes:
+1 - Selecting all HTML elements with class 'panel' assign to panels.
+2 - Iterating and adding event listener to each panel, and 'active' class to current 'panel' element.
+3 - Removes the 'active' class from all the panels
+*/
